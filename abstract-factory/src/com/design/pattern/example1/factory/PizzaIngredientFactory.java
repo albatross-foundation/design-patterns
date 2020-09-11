44b7909b3b3a3bd467625be124a68c4b5bbc15e9
ManhTV7 <manhtv7@vingroup.net>
2020-09-11 11:37:16 +0700

    add ingredient common

---
+++ abstract-factory/src/com/design/pattern/example1/factory/PizzaIngredientFactory.java
@@ -0,0 +1,17 @@
+package com.design.pattern.example1.factory;
+
+import com.design.pattern.example1.ingredient.*;
+
+public interface PizzaIngredientFactory {
+    public Dough createDough();
+
+    public Sauce createSauce();
+
+    public Cheese createCheese();
+
+    public Veggies[] createVeggies();
+
+    public Pepperoni createPepperoni();
+
+    public Clams createClams();
+}
