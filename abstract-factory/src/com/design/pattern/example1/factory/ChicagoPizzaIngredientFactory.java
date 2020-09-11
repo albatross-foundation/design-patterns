521c41bc1f602f5cb22b5fd888f2d0037a19244e
ManhTV7 <manhtv7@vingroup.net>
2020-09-11 12:01:58 +0700

    add chicago ingredient factory

---
+++ abstract-factory/src/com/design/pattern/example1/factory/ChicagoPizzaIngredientFactory.java
@@ -0,0 +1,38 @@
+package com.design.pattern.example1.factory;
+
+import com.design.pattern.example1.ingredient.*;
+import com.design.pattern.example1.ingredient.chicago.*;
+import com.design.pattern.example1.ingredient.common.SlicedPepperoni;
+
+public class ChicagoPizzaIngredientFactory implements PizzaIngredientFactory {
+    @Override
+    public Dough createDough() {
+        return new ThickCrustDough();
+    }
+
+    @Override
+    public Sauce createSauce() {
+        return new PlumTomatoSauce();
+    }
+
+    @Override
+    public Cheese createCheese() {
+        return new MozzarellaCheese();
+    }
+
+    @Override
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = {new BlackOlives(), new EggPlant(), new Spinach()};
+        return veggies;
+    }
+
+    @Override
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    @Override
+    public Clams createClams() {
+        return new FrozenClams();
+    }
+}
