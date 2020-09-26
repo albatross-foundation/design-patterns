b947f79c019c40fe0ba14c7a0ffdf8b5d9937591
ManhTV7 <manhtv7@vingroup.net>
2020-09-26 23:35:50 +0700

    change code

--- adapter/src/com/design/pattern/example1/DuckTest.java
+++ adapter/src/com/design/pattern/example1/DuckTest.java
@@ -2,8 +2,6 @@ package com.design.pattern.example1;
 
 public class DuckTest {
     public static void main(String[] arg){
-        MallardDuck mallardDuck = new MallardDuck();
-
         WildTurkey wildTurkey = new WildTurkey();
 
         Duck turkeyAdapter = new TurkeyAdapter(wildTurkey);
@@ -12,11 +10,7 @@ public class DuckTest {
         wildTurkey.gobble();
         wildTurkey.fly();
 
-        System.out.println("Duck say...");
-        mallardDuck.quack();
-        mallardDuck.fly();
-
-        System.out.println("TurkeyAdapter say");
+        System.out.println("TurkeyAdapter convert turkey to duck say ...");
         turkeyAdapter.quack();
         turkeyAdapter.fly();
     }
--- adapter/src/com/design/pattern/example1/WildTurkey.java
+++ adapter/src/com/design/pattern/example1/WildTurkey.java
@@ -3,11 +3,11 @@ package com.design.pattern.example1;
 public class WildTurkey implements Turkey {
     @Override
     public void gobble() {
-        System.out.println("Gobble");
+        System.out.println("Gobble from WildTurkey class");
     }
 
     @Override
     public void fly() {
-        System.out.println("Fly short");
+        System.out.println("Fly from WildTurkey class");
     }
 }
